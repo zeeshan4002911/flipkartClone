d733f340de7a5c0e0a04e8e4c20147ed1e9a7e5d
AAA4002911 <zeeshan4002911@gmail.com>
2023-04-23 13:03:23 +0530

    route added

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
-const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require("dotenv").config();
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const connection = require("./database/db.js");
 const defaultData = require("./defaultData.js");
 
-const PORT = process.env.PORT || 3001;
 connection();
 
+app.use('/', require('./routes/route.js'));
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log("server is up at port:", PORT));
 
 defaultData();
